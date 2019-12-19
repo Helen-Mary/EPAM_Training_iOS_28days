f039ef0e7dfa5e084ab50d73cfbc55534141cba0
Mary Priamuhina <elenka814@gmail.com>
2019-12-19 19:55:18 +0200

    first commit

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Helen-Mary on 18.12.2019.
+//  Copyright © 2019 EPAM_Training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
